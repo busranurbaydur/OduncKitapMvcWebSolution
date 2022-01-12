5dc4edcf7648fb4408f9c7a78ec12a2f366f3e84
busranurbaydur <busranurbaydur@outlook.com>
2022-01-12 14:57:16 +0300

    Kitap İşlemleri Yapıldı

---
+++ OduncKitapMvcWebSolution_Business/Managers/KitapManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OduncKitapMvcWebSolution_Business.Managers
+{
+    public class KitapManager
+    {
+
+        OduncKitapDBEntities dbContext = new OduncKitapDBEntities();
+
+        public List<Kitaplar> TumAktifKitaplariGetir()
+        {
+            try
+            {
+                List<Kitaplar> kitaplar = new List<Kitaplar>();
+                //page işlemi eklenecek
+
+                kitaplar = dbContext.Kitaplar.Where(x => x.SilindiMi == false && x.StokAdedi > 0).ToList();
+
+                return kitaplar;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ OduncKitapMvcWebSolution_UI/Controllers/KitapController.cs
@@ -0,0 +1,38 @@
+﻿using OduncKitapMvcWebSolution_Business;
+using OduncKitapMvcWebSolution_Business.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OduncKitapMvcWebSolution_UI.Controllers
+{
+    public class KitapController : Controller
+    {
+        KitapManager myKitapManager = new KitapManager();
+
+        // GET: Kitap
+        public ActionResult Index()
+        {
+            try
+            {
+                List<Kitaplar> kitapList = myKitapManager.TumAktifKitaplariGetir();
+                ViewBag.KitapListCount = 0;
+                if (kitapList.Count>0)
+                {
+                    ViewBag.KitapListCount = kitapList.Count;
+                }
+
+                return View(kitapList);
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.HataMesaji = "Beklenmedik bir hata oluştu.." ;
+                return View();
+                //ex.message loglanabilir..
+            }
+        }
+    }
+}
